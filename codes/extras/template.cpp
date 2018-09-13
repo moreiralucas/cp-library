cb8bb001615e51481f11dc8e5dd3bad86592eb58
Lucas Moreira <moreiralucascc@gmail.com>
2018-09-13 14:32:21 -0300

    First commit

---
+++ codes/extras/template.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+ 
+bool DEBUG = false;
+// #define int long long
+#define print if (DEBUG) std::cout <<
+#define ff first
+#define ss second
+#define pii pair<int, int>
+#define mp make_pair
+#define pb push_back
+#define vi vector<int>
+#define INF (int)(1e9*2)
+#define SYNC ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+using namespace std;
+ 
+int32_t main() {
+	SYNC;
+	// Code
+    return 0;
+}
